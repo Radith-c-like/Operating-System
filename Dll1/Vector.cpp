f61c5e167a18f6b9738883369734578a16e1fce7
Rodion <Rodion220006@gmail.com>
2025-10-31 10:25:07 -0700

    add Windows lib and dll

---
+++ Dll1/Vector.cpp
@@ -0,0 +1,27 @@
+﻿#include "pch.h"
+#include "Vector.h"
+// Глобальные векторы
+Vector g_origin(0.0, 0.0);
+Vector g_one_one(1.0, 1.0);
+
+Vector::Vector() : m_x(0.0), m_y(0.0) {}
+Vector::Vector(double x, double y) : m_x(x), m_y(y) {}
+
+double Vector::getX() const { return m_x; }
+double Vector::getY() const { return m_y; }
+
+double Vector::length() const {
+    return std::sqrt(m_x * m_x + m_y * m_y);
+}
+
+double Vector::angle() const {
+    return std::atan2(m_y, m_x);
+}
+
+Vector Vector::add(const Vector& other) const {
+    return Vector(m_x + other.m_x, m_y + other.m_y);
+}
+
+void Vector::print() const {
+    std::cout << "(" << m_x << ", " << m_y << ")\n";
+}
---
+++ Number/Number.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"  // если используется в проекте Visual Studio
+#include "Number.h"
+#include <stdexcept>
+
+Number::Number() : m_value(0.0) {}
+
+Number::Number(double value) : m_value(value) {}
+
+double Number::getValue() const {
+    return m_value;
+}
+
+void Number::setValue(double value) {
+    m_value = value;
+}
+
+Number Number::operator+(const Number& other) const {
+    return Number(m_value + other.m_value);
+}
+
+Number Number::operator-(const Number& other) const {
+    return Number(m_value - other.m_value);
+}
+
+Number Number::operator*(const Number& other) const {
+    return Number(m_value * other.m_value);
+}
+
+Number Number::operator/(const Number& other) const {
+    if (other.m_value == 0.0)
+        throw std::runtime_error("Division by zero");
+    return Number(m_value / other.m_value);
+}
+
+void Number::print() const {
+    std::cout << m_value << std::endl;
+}
+
+// Глобальные переменные
+Number NumberZero(0.0);
+Number NumberOne(1.0);
+
+// Функция создания числа
+Number CreateNumber(double value) {
+    return Number(value);
+}
