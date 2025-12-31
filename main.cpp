b07edd27f7d46d0affd8b9096a2a892612d59f1f
Rodion <Rodion220006@gmail.com>
2025-12-31 13:33:31 +0000

    add lab-5 from linux

--- main.cpp
+++ main.cpp
@@ -5,9 +5,8 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-const int N = 15; // Ваш номер в списке
+const int N = 22;
 
-// Функции-обработчики для каждого процесса
 void run_process_M() {
     std::string line;
     if (std::getline(std::cin, line)) {
@@ -66,10 +65,9 @@ void run_process_S() {
     }
 }
 
-// Вспомогательная функция для создания процесса в цепочке
 void spawn(void (*func)(), int in_fd, int out_fd) {
     pid_t pid = fork();
-    if (pid == 0) { // Дочерний процесс
+    if (pid == 0) {
         if (in_fd != STDIN_FILENO) {
             dup2(in_fd, STDIN_FILENO);
             close(in_fd);
@@ -89,28 +87,23 @@ int main() {
     std::getline(std::cin, input);
     input += "\n";
 
-    // Пайпы: fd[0] - чтение, fd[1] - запись
     int p1[2], p2[2], p3[2], p4[2], p5[2];
     pipe(p1); pipe(p2); pipe(p3); pipe(p4); pipe(p5);
 
-    // Запускаем цепочку
     spawn(run_process_M, p1[0], p2[1]);
     spawn(run_process_A, p2[0], p3[1]);
     spawn(run_process_P, p3[0], p4[1]);
     spawn(run_process_S, p4[0], p5[1]);
 
-    // Закрываем ненужные концы пайпов в родителе
     close(p1[0]); 
     close(p2[0]); close(p2[1]);
     close(p3[0]); close(p3[1]);
     close(p4[0]); close(p4[1]);
     close(p5[1]);
 
-    // Отправляем данные
     write(p1[1], input.c_str(), input.length());
-    close(p1[1]); // Сигнал конца данных
+    close(p1[1]);
 
-    // Читаем результат
     char buffer[256];
     ssize_t bytes = read(p5[0], buffer, sizeof(buffer) - 1);
     if (bytes > 0) {
@@ -120,7 +113,6 @@ int main() {
 
     close(p5[0]);
 
-    // Ждем завершения всех детей
     for(int i = 0; i < 4; i++) wait(NULL);
 
     return 0;
